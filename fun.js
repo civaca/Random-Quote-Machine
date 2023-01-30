c5eb9be2d243ee4ba950a9c537aadf0da1e439e9
Cristian Vaca <civaca7@gmail.com>
2023-01-29 23:07:23 -0500

    More changes

--- fun.js
+++ fun.js
@@ -1,4 +1,4 @@
-
+const { useState } = React
 const changeColor=()=>{
   let x=Math.floor(Math.random()*256);
   document.body.style.background="rgb("+x+",241,237)"
@@ -6,32 +6,28 @@ const changeColor=()=>{
 
 
 function QUOTES2 (){
-const[qt,setQt]=useState("gg");
+const[qt,setQt]=useState({author:"",text:""});
 
 return (
   <div>
-    <p id="text">{qt}</p>
-    <p id="author">{qt}</p>
+    <p id="text">{qt.author}</p>
+    <p id="author">{qt.text}</p>
     
     <span class="con" >
       <a  href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">
       <i class="fa fa-twitter-square btn btn-primary fa-2x " aria-hidden="true"></i> 
     </a>
-    <button id="new-quote" class="btn btn-primary">Change Quote</button>
+    <button id="new-quote" class="btn btn-primary" onClick={()=>}>Change Quote</button>
     </span>
    
   </div>
 );
 }
 
-function FavoriteColor() {
-  const [color, setColor] = useState("red");
 
-  return <h1>My favorite color is {color}!</h1>
-}
 
  
 
 const root=ReactDOM.createRoot(document.getElementById("quote-box"));
-root.render(<FavoriteColor />)
+root.render(<QUOTES2 />)
 
