b6760d8f16197b711ec411d699395b9de629ccde
Cristian Vaca <civaca7@gmail.com>
2023-01-29 22:39:09 -0500

    hoos

--- fun.js
+++ fun.js
@@ -5,48 +5,33 @@ const changeColor=()=>{
 };
 
 
-class QUOTES extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      author:"",
-      text: ""
-    }
-    this.handleClick=this.handleClick.bind(this)
-  }
-  
-  handleClick(){
-    fetch("https://api.quotable.io/random")
-    .then(response=>response.json())
-    .then(data=>this.setState({
-      author:data.author,
-      text:data.content
-    }))
-    changeColor();
-  }
-  componentDidMount(){
-    this.handleClick();
-   }
-  render(){
-    return (
-      <div>
-        <p id="text">{this.state.text}</p>
-        <p id="author">-{this.state.author}</p>
-        
-        <span class="con" >
-          <a  href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">
-          <i class="fa fa-twitter-square btn btn-primary fa-2x " aria-hidden="true"></i> 
-        </a>
-        <button id="new-quote" onClick={this.handleClick} class="btn btn-primary">Change Quote</button>
-        </span>
-       
-      </div>
-    )
-  }
+function QUOTES2 (){
+const[qt,setQt]=useState("gg");
+
+return (
+  <div>
+    <p id="text">{qt}</p>
+    <p id="author">{qt}</p>
+    
+    <span class="con" >
+      <a  href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">
+      <i class="fa fa-twitter-square btn btn-primary fa-2x " aria-hidden="true"></i> 
+    </a>
+    <button id="new-quote" class="btn btn-primary">Change Quote</button>
+    </span>
+   
+  </div>
+);
+}
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return <h1>My favorite color is {color}!</h1>
 }
 
  
 
 const root=ReactDOM.createRoot(document.getElementById("quote-box"));
-root.render(<QUOTES/>)
+root.render(<FavoriteColor />)
 
